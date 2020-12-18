c9af3edb897b8d8e6eea394aa37392dc42998be5
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2020-12-18 16:01:13 -0300

    Segunda parte

---
+++ Ex2/ex2.js
@@ -0,0 +1,83 @@
+/*
+function returnEmployee (){
+    const name= "Francis";
+    const age= 21;
+    const position= "Manager";
+
+    console.log (`His name is ${name}, he is ${age} years old and he works as %{position}`)
+
+}
+returnEmployee();
+
+function request (page,id){
+    const URL= `http://server.com/${page}/${id}`;
+    console.log(URL)
+}
+
+request('pages',5)
+
+//take a random numer to this array
+const names= ["James","Ron","Ace"];
+
+console.log(`The name is ${names[Math.floor(Math.random() *names.lenght - 1) +1 ]}`)
+
+//ppode fazer calculos
+console.log(`The numerber is ${ 5 +1 }`)
+
+//fazer com functions
+function car({
+    return 'car'
+}
+
+console.log(`The numerber is ${ car()}`
+
+//Fazer referencia para um objeto
+
+const user = {
+    name: "Francis",
+    age: 21,
+    position: "Manager"
+}
+console.log (`My name is ${user.name}, I am ${user.age}, I work as ${user.position}`)
+
+
+//164
+
+    const name= "ludimilla";
+    let age= 18;
+    let position= "Web developer";
+    let salary= 1350;
+
+    console.log (`My name is ${name}, I'm ${age} years old, I work as ${position} and I make $ ${salary} reias per month.`)
+
+const artists =['BTS', 'ONE DIRECTION','ED SHEERAN', 'NU']
+
+artists.forEach(function(artist){
+    console.log(artist)
+    
+})
+
+const names =['ludi', 'mari', 'isa', 'belinha'];
+let template = '';
+
+names.forEach(function(name){
+    template =+ `<div> Hi, my name is ${name}</div>`
+});
+
+document.body.insertAdjacentHTML ("afterbegin", template)
+
+
+const purchases = [
+    {quantity:2, amount:100},
+    {quantity:5, amount:100},
+    {quantity:10, amount:100}
+]
+
+let total = 0;
+
+purchases.forEach(function(item){
+    total += item.quantity * item.amount
+})
+
+console.log(total)
+*/
